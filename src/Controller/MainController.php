d05d2323cd7306a37a152050e06026542134d095
aurelius85 <aurel095@hotmail.com>
2022-02-19 09:47:21 +0100

    Establishment of assets (button, variable, color) + footer, header + style home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,23 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Entity\Promotion;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController
 {
-    public function index(): Response
+    /**
+     * @Route("/", name="www_home")
+     * @Template("www/index.html.twig")
+     */
+    public function index(): array
     {
-        $product1 = new Product('Cuve à gasoil', 250000, 'Farmitoo');
-        $product2 = new Product('Nettoyant pour cuve', 5000, 'Farmitoo');
-        $product3 = new Product('Piquet de clôture', 1000, 'Gallagher');
-
-        $promotion1 = new Promotion(50000, 8, false);
-
-        // Je passe une commande avec
-        // Cuve à gasoil x1
-        // Nettoyant pour cuve x3
-        // Piquet de clôture x5
-
-        return new Response();
+        return [];
     }
 }
